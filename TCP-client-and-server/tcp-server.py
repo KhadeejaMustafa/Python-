05c72ade5124aa5cbbdc5e72da45a94a342bf861
Kdeeja_Mstafa <118957732+KhadeejaMustafa@users.noreply.github.com>
2024-09-28 21:34:49 +0300

    Update tcp-server.py

--- TCP-client-and-server/tcp-server.py
+++ TCP-client-and-server/tcp-server.py
@@ -1,4 +1,4 @@
-# this program will we used to develop a TCP server, using socket library in Python.
+# developing a TCP server using socket library in Python.
 # socket methods used: .sock(), .gethostname(), .bind(), .listen(), .accept()
 
 import socket
@@ -6,7 +6,7 @@ import socket
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # creating 'serverSocket' object. AF_NET = IPv4, SOCK_STREAM = TCP
 
 host = socket.gethostname()
-port = 80
+port = 444
 
 serverSocket.bind((host, port))
 serverSocket.listen(4)
@@ -15,6 +15,6 @@ while True:
     clientSocket, address = serverSocket.accept()
     print(f'Recieved connection from {str(address)}')
 
-    messageToClient = "Thank you for establishing connection with " + host
-    clientSocket.send(messageToClient)
+    message = "Thank you for establishing connection with " + host + "\r\n"
+    clientSocket.send(message.encode('ascii'))
     clientSocket.close()
